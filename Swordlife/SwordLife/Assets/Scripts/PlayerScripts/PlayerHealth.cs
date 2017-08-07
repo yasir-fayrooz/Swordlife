f227b1f9da49f4c349341582270ad396fce4a396
Crazeyheadshot <yasir.fayrooz@hotmail.com>
2017-08-07 15:51:41 +1000

    Added functional HP Bar. (Slightly buggy will fix later)

---
+++ Swordlife/SwordLife/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 3;
+    public int currentHealth;
+    
+	// Use this for initialization
+	void Start () {
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentHealth > startingHealth)
+        {
+            currentHealth = startingHealth;
+        }
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+	}
+
+    public void Damage(int dmg)
+    {
+        currentHealth -= dmg;
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
