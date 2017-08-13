22c82bea5659ed7216acef4a30f8976023727aae
Crazeyheadshot <yasir.fayrooz@hotmail.com>
2017-08-13 20:38:29 +1000

    Scaling resolution/screen lock, New apk for testing

--- Swordlife/SwordLife/Assets/Scripts/HUD/HUD.cs
+++ Swordlife/SwordLife/Assets/Scripts/HUD/HUD.cs
@@ -3,21 +3,33 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class HUD : MonoBehaviour {
+public class HUD : MonoBehaviour
+{
 
     public Sprite[] HeartSprites;
     public Image HeartUI;
     private PlayerHealth playerHP;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         playerHP = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
     }
-	
-    void Update() 
-    {
 
+    void Update()
+    {
+        ScreenRotate();
         HeartUI.sprite = HeartSprites[playerHP.currentHealth];
     }
 
+    void ScreenRotate()
+    {
+        if (Input.deviceOrientation == DeviceOrientation.FaceDown ||
+            Input.deviceOrientation == DeviceOrientation.FaceUp ||
+            Input.deviceOrientation == DeviceOrientation.Portrait)
+        {
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+        }
+
+    }
 }
--- Swordlife/SwordLife/Assets/Scripts/PlayerScripts/HitDetection.cs
+++ Swordlife/SwordLife/Assets/Scripts/PlayerScripts/HitDetection.cs
@@ -5,13 +5,11 @@ using UnityEngine.UI;
 
 public class HitDetection : MonoBehaviour {
 
-    private GameObject Player;
     private PlayerHealth PlayerHP;
 
     // Use this for initialization
     void Start () 
 	{
-        Player = GameObject.FindGameObjectWithTag("Player");
         PlayerHP = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
     }
     void OnTriggerEnter2D(Collider2D col)
