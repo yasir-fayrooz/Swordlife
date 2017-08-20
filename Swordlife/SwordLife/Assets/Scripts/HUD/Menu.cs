9a3f11815b46affa5ddb9dccdaec54b392db495d
Crazeyheadshot <yasir.fayrooz@hotmail.com>
2017-08-20 13:47:42 +1000

    Placeholder main menu, Background music ingame.

---
+++ Swordlife/SwordLife/Assets/Scripts/HUD/Menu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject mainMenuHolder;
+    public GameObject optionsMenuHolder;
+
+    public Slider[] volumeSliders;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        mainMenuHolder.SetActive(false);
+        optionsMenuHolder.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        mainMenuHolder.SetActive(true);
+        optionsMenuHolder.SetActive(false);
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        //AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+
+    }
+
+    public void SetSFXVolume(float value)
+    {
+
+    }
+}
