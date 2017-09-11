ba84e6513e64eee53407fc496087cc19152a8f96
Crazeyheadshot <yasir.fayrooz@hotmail.com>
2017-09-11 20:00:17 +1000

    New background colours change every 15 points

--- Swordlife/SwordLife/Assets/Scripts/HUD/BackGroundScript.cs
+++ Swordlife/SwordLife/Assets/Scripts/HUD/BackGroundScript.cs
@@ -16,14 +16,25 @@ public class BackGroundScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-
-        if(scoreCount.score == 10)
+        if (scoreCount.score == 15)
         {
-                myAnimator.Play("Background_Animation2");
+            myAnimator.Play("Background_Animation2");
         }
-        else if(scoreCount.score == 20)
+        else if(scoreCount.score == 30)
         {
             myAnimator.Play("Background_Animation3");
         }
-   }
+        else if (scoreCount.score == 45)
+        {
+            myAnimator.Play("Background_Animation4");
+        }
+        else if (scoreCount.score == 60)
+        {
+            myAnimator.Play("Background_Animation5");
+        }
+        else if (scoreCount.score == 75)
+        {
+            myAnimator.Play("Background_Animation5");
+        }
+    }
 }
