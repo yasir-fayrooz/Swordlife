0d25c1fed661847ef4a44b1d8fc2a593c647fa69
Crazeyheadshot <yasir.fayrooz@hotmail.com>
2017-09-11 17:29:31 +1000

    Added placeholder background color changes

---
+++ Swordlife/SwordLife/Assets/Scripts/HUD/BackGroundScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScript : MonoBehaviour {
+
+    private Animator myAnimator;
+    private scoreCount scoreCount;
+
+	// Use this for initialization
+	void Start () {
+        myAnimator = gameObject.GetComponent<Animator>();
+        scoreCount = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<scoreCount>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if(scoreCount.score == 10)
+        {
+                myAnimator.Play("Background_Animation2");
+        }
+        else if(scoreCount.score == 20)
+        {
+            myAnimator.Play("Background_Animation3");
+        }
+   }
+}
